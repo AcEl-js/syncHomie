f4c8d5eaa53f86db10985cf262d9ab02366f0878
AcEl-js <achraf2elhachemy@gmail.com>
2025-04-27 20:06:44 +0100

    add the rating stars to mediatwo page

--- src/app/mediatwo/page.tsx
+++ src/app/mediatwo/page.tsx
@@ -18,6 +18,7 @@ import { Bell, Eye, Plus } from "lucide-react"
 import { Progress } from "@/components/ui/progress"
 import { Textarea } from "@/components/ui/textarea"
 import { Badge } from "@/components/ui/badge"
+import StarRating from "./rating";
 
 interface Show {
     title: string
@@ -126,23 +127,11 @@ const shows: Show[] = [
             <div className="flex items-center justify-between">
             
               <div className="flex">
-                {[1, 2, 3, 4, 5].map((star) => (
-                  <svg
-                    key={star}
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="text-gray-500"
-                  >
-                    <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
-                  </svg>
-                ))}
+                
+              <div>
+                
+                <StarRating rating={3.5} />
+              </div>
               </div>
             </div>
 
